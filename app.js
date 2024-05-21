a80e710a511f843c7a6672d035c3ce0e09fbb82f
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-21 15:30:08 +0500

    Bulb Assignment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const img = document.querySelector(`img`)
 
 
 img.addEventListener(`mouseover` , function() {
-    img.src =  `./assets/Bulb On.png`
+    img.src =  `https://i.pinimg.com/736x/8b/e0/60/8be0603fa9222908eac38746f7c72c2a.jpg`
 
 })
 
